522eecd89d580c49d8f8c669b77b76662883ae57
anshitaagrawal09 <anshitaagrawal75@gmail.com>
2025-03-03 15:14:02 +0530

    UC1

---
+++ src/main/java/com/example/Greeting/controller/GreetingController.java
@@ -0,0 +1,29 @@
+package com.example.Greeting.controller;
+
+import com.example.Greeting.model.Greeting;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greetings")
+public class GreetingController {
+
+    @GetMapping("/{name}")
+    public Greeting getGreeting(@PathVariable String name) {
+        return new Greeting("Hello, " + name + "!");
+    }
+
+    @PostMapping
+    public Greeting createGreeting(@RequestBody String name) {
+        return new Greeting("Greeting created for " + name);
+    }
+
+    @PutMapping("/{name}")
+    public Greeting updateGreeting(@PathVariable String name) {
+        return new Greeting("Greeting updated for " + name);
+    }
+
+    @DeleteMapping("/{name}")
+    public Greeting deleteGreeting(@PathVariable String name) {
+        return new Greeting("Greeting deleted for " + name);
+    }
+}
---
+++ src/main/java/com/example/Greeting/model/Greeting.java
@@ -0,0 +1,17 @@
+package com.example.Greeting.model;
+
+public class Greeting {
+    private String message;
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
